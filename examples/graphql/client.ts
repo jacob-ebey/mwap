2895640ae2c213b1d172f809e219114f4fe3c49b
tundera <tunderadev@gmail.com>
2021-05-09 20:17:33 -0600

    misc cleanup

--- examples/graphql/client.ts
+++
@@ -1,5 +0,0 @@
-import { hydrate } from "@mwap/client";
-
-console.log("Hello from custom client entrypoint!");
-
-hydrate();
--- examples/graphql/lib/graphql/client.ts
+++
@@ -1,5 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-export const client = new GraphQLClient(
-  "https://countries.trevorblades.com/graphql"
-);
